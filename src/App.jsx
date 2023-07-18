17ab648fe59d43008248f055ab0767dd93d0498b
Dvzeira <daviandrade062@gmail.com>
2023-07-18 15:05:34 -0300

    pokemons add

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react"
+
+async function fetchPokemon() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1080")
+  const data = await response.json()
+  return data.results
+}
+
+function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [pokemonShown, setPokemonShown] = useState(null)
+
+
+  if (pokemon.length === 0) {
+    fetchPokemon().then(resuls => {
+      console.log("state change")
+      console.log(resuls)
+      setPokemon(resuls)
+    })
+  }
+
+  const showDetails = async (url) => {
+    const data = await fetch(url).then(res => res.json())
+    console.log(data)
+    setPokemonShown(data)
+  }
+
+  return (
+    <div className="app">
+
+      <div className="pokesInterface">
+        <h2>Pokédex</h2>
+        <ul className="pokemon">
+          {pokemon.map(mon => (
+            <li key={mon.name}>
+              <span>{mon.name}</span>
+              <button onClick={() => showDetails(mon.url)}>
+                Ver detalhes
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {pokemonShown && (
+        <div className="infoPokes">
+          <h2>{pokemonShown.name}</h2>
+          <img
+            src={pokemonShown.sprites.front_default}
+            alt=""
+          />
+          <div className="stat">
+            <b>Tipo: </b>
+            {pokemonShown.types.map(({ type }) => (
+              <span key={type.name}>{type.name} </span>
+            ))}
+          </div>
+          <div className="stat">
+            <b>Altura: </b>{pokemonShown.height / 10} m
+          </div>
+          <div className="stat">
+            <b>Peso: </b>{pokemonShown.weight / 10} Kg
+          </div>
+          <div className="stat">
+            <b>Atributos</b>
+            <ul>
+              {pokemonShown.stats.map(({ base_stat, stat }) => (
+                <li key={stat.name}>
+                  {stat.name}: {base_stat}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="stat">
+
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
